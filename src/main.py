ccbacfe6c4fb1d371671595638d0bef12d75f6ce
Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>
2019-10-31 13:44:49 +0000

    main.py now showcases watson NLU

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
-import spotify_interface
-import track
+import spotify_interface as s_int
+import watson_nlu_interface as w_int
 
 def main():
-    si = spotify_interface.spotify_interface()
+    si = s_int.spotify_interface()
+    wi = w_int.watson_nlu_interface()
+    phrase = "This phrase is very positive."
+    
     si.request_token()
     tracks = si.search_tracks(["circles", "post", "malone"], 10)
     for i in tracks:
         print("Name: ", i.name, "  Artists: ", i.artists, "  Duration: ", i.duration_readable())
+    txt_att = wi.query_text_analyzer(phrase)
+    print("For phrase: '{0}' the key terms are: {1} and the overall sentiment is ~{2:.2f}".format(phrase, txt_att.key_terms, txt_att.overall_sentiment))
     
 if __name__ == "__main__":
     main()
