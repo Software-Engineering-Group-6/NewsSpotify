f0b3cc7af3362a7db84604d55446d0d6a6484f1a
Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>
2019-11-01 18:23:33 +0000

    finished unit testing for tracks

--- tests/unit/test_track.py
+++ tests/unit/test_track.py
@@ -13,7 +13,7 @@ import track
 #       name = "Mr. Brightside"
 #       popularity = 76
 #       preview_url = ""
-#       external_url = "https://open.spotify.com/artist/0C0XlULifJtAgn6ZNCW2eu"
+#       external_url = "https://open.spotify.com/track/3n3Ppam7vgaVa1iaRUc9Lp"
 #       duration_readable() = "3:42"
     
 # track_normal_2.json =>
@@ -43,7 +43,7 @@ class test_basic(unittest.TestCase):
         self.f_norm_2 = None
         self.f_corr_1 = None
         self.f_corr_2 = None
-    
+        
         # Load test fixture data
         # Normal and valid JSON first
         try:
@@ -81,6 +81,41 @@ class test_basic(unittest.TestCase):
             f.close()
     
     def test_track_init(self):
+        # build expected data objects
+        # track_normal_1.json
+        exp_norm_1 = {}
+        exp_norm_1["artists"] = ["The Killers"]
+        exp_norm_1["duration"] = 222200
+        exp_norm_1["name"] = "Mr. Brightside"
+        exp_norm_1["popularity"] = 76
+        exp_norm_1["preview_url"] = ""
+        exp_norm_1["external_url"] = "https://open.spotify.com/track/3n3Ppam7vgaVa1iaRUc9Lp"
+        exp_norm_1["duration_readable"] = "3:42"
+        
+        # track_normal_2.json
+        exp_norm_2 = {}
+        exp_norm_2["artists"] = ["Carly Rae Jepsen"]
+        exp_norm_2["duration"] = 207959
+        exp_norm_2["name"] = "Cut To The Feeling"
+        exp_norm_2["popularity"] = 7
+        exp_norm_2["preview_url"] = ""
+        exp_norm_2["external_url"] = "https://open.spotify.com/track/11dFghVXANMlKmJXsNCbNl"
+        exp_norm_2["duration_readable"] = "3:27"
+        
+        # track_corrupt_1.json
+        exp_corr_1 = {}
+        exp_corr_1["artists"] = []
+        exp_corr_1["duration"] = 0
+        exp_corr_1["name"] = ""
+        exp_corr_1["popularity"] = 0
+        exp_corr_1["preview_url"] = ""
+        exp_corr_1["external_url"] = ""
+        exp_corr_1["duration_readable"] = "0:00"
+        
+        # track_corrupt_2.json - we expect the same output as with
+        # track_corrupt_1.json
+        exp_corr_2 = exp_corr_1 
+        
         # Build normal tracks first
         t_norm_1 = track.Track(self.f_norm_1)
         t_norm_2 = track.Track(self.f_norm_2)
@@ -101,6 +136,26 @@ class test_basic(unittest.TestCase):
         self.assertTrue(isinstance(t_norm_2, track.Track))
         self.assertTrue(isinstance(t_corr_1, track.Track))
         self.assertTrue(isinstance(t_corr_2, track.Track))
+        
+        # Checking correct field values for t_norm_1, t_norm_2 and t_corr_1
 
+        # t_norm_1
+        self.track_tester(t_norm_1, exp_norm_1)
+        # t_norm_2
+        self.track_tester(t_norm_2, exp_norm_2)
+        # t_corr_1
+        self.track_tester(t_corr_1, exp_corr_1)
+        # t_corr_2
+        self.track_tester(t_corr_2, exp_corr_2)
+
+    def track_tester(self, t, d):
+        self.assertEqual(t.artists, d["artists"])
+        self.assertEqual(t.duration, d["duration"])
+        self.assertEqual(t.name, d["name"])
+        self.assertEqual(t.popularity, d["popularity"])
+        self.assertEqual(t.preview_url, d["preview_url"])
+        self.assertEqual(t.external_url, d["external_url"])
+        self.assertEqual(t.duration_readable(), d["duration_readable"])
+        
 if __name__ == "__main__":
     unittest.main()
