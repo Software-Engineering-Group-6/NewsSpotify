071f259c323b723030d7cb6dfcc931218e9f3e2c
Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>
2019-11-12 20:56:48 +0000

    changed main.py to show current progress

--- src/main.py
+++ src/main.py
@@ -8,14 +8,11 @@ def main():
     si = s_int.spotify_interface()
     wi = w_int.watson_nlu_interface()
     ni = n_int.news_interface()
-    phrase = "This phrase is very positive."
     
     si.request_token()
     tracks = si.search_tracks(["circles", "post", "malone"], 10)
     for i in tracks:
         print("Name: ", i.name, "  Artists: ", i.artists, "  Duration: ", i.duration_readable())
-    txt_att = wi.query_text_analyzer(phrase)
-    print("For phrase: '{0}' the key terms are: {1} and the overall sentiment is ~{2:.2f}".format(phrase, txt_att.key_terms, txt_att.overall_sentiment))
     news = ni.get_breaking_news(10, ["BBC News"])
     for n in news:
         print("Source: {0}, Headline: {1}".format(n.source, n.headline))
